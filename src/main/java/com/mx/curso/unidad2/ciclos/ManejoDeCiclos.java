83e038bf0b9952937040d45168ef0eb696aae732
Antonio <ux24nt170@ux.edu.mx>
2025-11-22 08:08:39 -0600

    Ejercicios 22/11/2025

---
+++ src/main/java/com/mx/curso/unidad2/ciclos/ManejoDeCiclos.java
@@ -0,0 +1,17 @@
+package com.mx.curso.unidad2.ciclos;
+
+public class ManejoDeCiclos {
+
+
+    static void main(String[] args) {
+
+        int j = 1;
+        System.out.println("Tabla del 7");
+        while(j<=10){
+            System.out.println("7 * "+j+" = "+7*j);
+            j++;
+        }
+
+
+    }
+}
---
+++ src/main/java/com/mx/curso/unidad2/sentencias/OperacionMatematica.java
@@ -0,0 +1,15 @@
+package com.mx.curso.unidad2.sentencias;
+
+public class OperacionMatematica {
+
+    static void main(String[] args) {
+        int producto = 8*12;
+        int cociente = 12/4;
+
+        System.out.println("El valor del producto es: "+producto);
+        System.out.println("El valor del cociente es: "+cociente);
+
+
+    }
+
+}
